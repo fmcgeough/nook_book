3391f17a9f7259e8a487d62951503ca57a43521d
Frank McGeough <fmcgeough@pagerduty.com>
2020-09-01 16:06:16 -0400

    Remove variable

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,20 +4,6 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
-
-config :nook_book, NookBookWeb.Endpoint,
-  http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
-  ],
-  secret_key_base: secret_key_base
-
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
